5b57137c3d03f8a0d203ca7c9804facaa469b678
Marwah Inas <92029162+MarwahInas@users.noreply.github.com>
2023-12-31 14:49:42 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,16 @@
+<template>
+  <Product/>
+</template>
+
+<script>
+import Product from './components/Product.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Product,
+},
+};
+</script>
+
 
